e18ae8e43201d7f14ea09b2bfd7769fb5bd5a057
Oliver Cai <ocai2017@gmail.com>
2026-08-07 00:31:35 -0400

    verify demo

--- solana/programs/stable-swapper/src/lib.rs
+++ solana/programs/stable-swapper/src/lib.rs
@@ -12,7 +12,7 @@ use errors::*;
 use state::*;
 use utils::*;
 
-declare_id!("5y8vk3bkbNuahkfDF4ub53bcoszHEnhvz4oWRfH6S81h");
+declare_id!("Di4bHuP7xRMrpDuZBsQpgiGkn6dec52SQ1yX846sySDb");
 
 // NOTE: The previously deployed whitelist PDA (seeded b"address_whitelist") is orphaned
 // on devnet/mainnet after whitelist removal. Its rent is intentionally forfeited; adding a
